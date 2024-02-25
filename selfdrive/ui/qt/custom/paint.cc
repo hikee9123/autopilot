1f12a7f2f21bb99eb730cdf0a4aab6e82239bd45
hikee9123 <hikee9123@gmail.com>
2024-02-25 18:26:14 +0900

    test

--- selfdrive/ui/qt/custom/paint.cc
+++ selfdrive/ui/qt/custom/paint.cc
@@ -294,7 +294,7 @@ void OnPaint::drawLead(QPainter &p, const cereal::RadarState::LeadData::Reader &
       }
 
       szFont = 50;
-      rcText = QRect(x - (sz * 1.25), y - sz, 2 * (sz * 1.25),  sz );
+      rcText = QRect(x - (sz * 1.25), y - sz, 2 * (sz * 1.25),  sz * 1.25 );
       p.setBrush(QColor(218, 202, 37, 255));
       p.drawPolygon(polygonData.data(), polygonData.size());    
 
