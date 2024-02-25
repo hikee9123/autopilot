5333b467995823f94cf02132e703a10b2343f081
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-02-25 17:57:24 +0900

    Update paint.cc

--- selfdrive/ui/qt/custom/paint.cc
+++ selfdrive/ui/qt/custom/paint.cc
@@ -292,7 +292,7 @@ void OnPaint::drawLead(QPainter &p, const cereal::RadarState::LeadData::Reader &
       }
 
       szFont = 50;
-      rcText = QRect(x - (sz * 1.25), y, 2 * (sz * 1.25), 2 * (sz * 1.25) );
+      rcText = QRect(x - (sz * 1.25), y, 2 * (sz * 1.25),  sz );
       p.setBrush(QColor(218, 202, 37, 255));
       p.drawPolygon(polygonData.data(), polygonData.size());    
 
