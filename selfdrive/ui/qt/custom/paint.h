78da0250c1de355e0d4c94dace6f56811c697b32
hikee9123 <hikee9123@gmail.com>
2024-02-25 10:05:23 +0900

    fix

--- selfdrive/ui/qt/custom/paint.h
+++ selfdrive/ui/qt/custom/paint.h
@@ -53,6 +53,10 @@ private:
 private:
   void   ui_main_navi( QPainter &p );
 
+private:
+  inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
+  inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
 
 private:
   UIState  *state;
