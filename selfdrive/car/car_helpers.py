c1e87ae5758bf310591c2d4fece244bc4e5aba84
hikee9123 <hikee9123@gmail.com>
2024-03-22 20:04:35 +0900

    fix

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -22,7 +22,7 @@ EventName = car.CarEvent.EventName
 def get_startup_event(car_recognized, controller_available, fw_seen):
   build_metadata = get_build_metadata()
   #if build_metadata.openpilot.comma_remote and build_metadata.tested_channel:
-  if build_metadata.tested_channel:
+  if build_metadata.tested_channel:  #custom
     event = EventName.startup
   else:
     event = EventName.startupMaster
