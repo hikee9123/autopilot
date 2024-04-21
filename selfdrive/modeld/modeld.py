44bb345ae8405128f02dde041482e2f01059421f
hikee9123 <hikee9123@gmail.com>
2024-04-21 09:56:17 +0900

    test

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -55,25 +55,28 @@ class ModelState:
 
     print(f"Custom Param  JSON document  model name{model_name}")
     print(f"model name1 = {MODEL_PATHS}")
-
     if model_name ==  "1.Certified_Herbalist1,supercombo_CH1":
-      MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / 'models/supercombos/supercombo_CH1.onnx'
+      self.supercombo_name = 'models/supercombos/supercombo_CH1.onnx'
       print("model name = CH1 model")
     elif model_name ==   "2.Certified_Herbalist2,supercombo_CH2": 
-      MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / 'models/supercombos/supercombo_CH2.onnx'
+      self.supercombo_name = 'models/supercombos/supercombo_CH2.onnx'
       print("model name = CH2 model")
     elif model_name ==   "3.Los_Angeles model,supercombo_LA":
-      MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / 'models/supercombos/supercombo_LA.onnx'
+      self.supercombo_name = 'models/supercombos/supercombo_LA.onnx'
       print("model name = LA model")
     elif model_name ==   "4.Recertified_Herbalist,supercombo_RH":
-      MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / 'models/supercombos/supercombo_RH.onnx'
+      self.supercombo_name = 'models/supercombos/supercombo_RH.onnx'
       print("model name = RH model")
     elif model_name == "5.Duck_Amigo model,supercombo_DA":
-      MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / 'models/supercombos/supercombo_DA.onnx'
+      self.supercombo_name = 'models/supercombos/supercombo_DA.onnx'
       print("model name = DA model")
     elif model_name == "6.WD40 model,supercombo_WD40":
-      MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / 'models/supercombos/supercombo_WD40.onnx'
+      self.supercombo_name = 'models/supercombos/supercombo_WD40.onnx'
       print("model name = WD40 model")
+    else:
+      self.supercombo_name = 'models/supercombo.onnx'
+    MODEL_PATHS[ModelRunner.ONNX] = Path(__file__).parent / self.supercombo_name
+    print(f"supercombo name = {self.supercombo_name =}")
     print(f"model name2 = {MODEL_PATHS}")
 
 
