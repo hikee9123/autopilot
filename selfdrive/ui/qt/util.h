fd67a5db82b5dd1f4310538c9ca2edc221497d43
home_bhcho <hikee9123.gmail.com>
2024-08-09 11:50:56 +0900

    fix

--- selfdrive/ui/qt/util.h
+++ selfdrive/ui/qt/util.h
@@ -28,6 +28,10 @@ QPixmap loadPixmap(const QString &fileName, const QSize &size = {}, Qt::AspectRa
 QPixmap bootstrapPixmap(const QString &id);
 bool hasLongitudinalControl(const cereal::CarParams::Reader &car_params);
 
+QColor interpColor(float xv, std::vector<float> xp, std::vector<QColor> fp);
+void drawRoundedRect(QPainter &painter, const QRectF &rect, qreal xRadiusTop, qreal yRadiusTop, qreal xRadiusBottom, qreal yRadiusBottom);
+
+
 struct InterFont : public QFont {
   InterFont(int pixel_size, QFont::Weight weight = QFont::Normal) : QFont("Inter") {
     setPixelSize(pixel_size);
