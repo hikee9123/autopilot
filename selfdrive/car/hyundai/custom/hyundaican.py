da34c39dd2415842a65525df971b44492321cfb7
home_bhcho <hikee9123.gmail.com>
2024-08-09 12:11:22 +0900

    fix

--- selfdrive/car/hyundai/custom/hyundaican.py
+++ selfdrive/car/hyundai/custom/hyundaican.py
@@ -45,7 +45,7 @@ def create_mdps12(packer, frame, mdps12):
   values["CF_Mdps_MsgCount2"] = frame % 0x100
   values["CF_Mdps_Chksum2"] = 0
 
-  dat = packer.make_can_msg("MDPS12", 2, values)[2]
+  dat = packer.make_can_msg("MDPS12", 2, values)[1]
   checksum = sum(dat) % 256
   values["CF_Mdps_Chksum2"] = checksum
 
