66473a0ccd149e149d68e47fb4a3c877d6c54f10
home_bhcho <hikee9123.gmail.com>
2024-08-10 15:13:27 +0900

    fix

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -83,7 +83,7 @@ class ModelState:
   def select_supercombo(self):
     model_name = Params().get("SelectedModel", encoding='utf-8')
 
-    print(f"model Custom Param  JSON document name=[{model_name}]")
+    print(f"model Custom Param  JSON document name = [{model_name}]")
     print(f"model name1 = [{MODEL_PATHS}]")
 
     model_map = {
