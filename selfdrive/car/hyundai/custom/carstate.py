bf373a4c79686f2b2dacc8fae6153b6925fba86e
home_bhcho <hikee9123.gmail.com>
2024-08-12 16:49:04 +0900

    fix

--- selfdrive/car/hyundai/custom/carstate.py
+++ selfdrive/car/hyundai/custom/carstate.py
@@ -207,6 +207,7 @@ class CarStateCustom():
       return
     
 
+    self.modelxDistance = self.NC.modelxDistance
     model_v2 = self.NC.sm['modelV2']
     self.desiredCurvature = model_v2.action.desiredCurvature 
     self.laneChangeState = model_v2.meta.laneChangeState
@@ -354,14 +355,11 @@ class CarStateCustom():
   def create_events(self, ret_cs, CS, events):
     if self.timer_resume > 0:
         self.timer_resume -= 1
-   #if self.NC == None:
-   #      modelxDistance = self.NC.modelxDistance
-    #else:
-    modelxDistance = 0
+
     v_ego_kph = self.clu_Vanz   # CS.cluster_speed
     if ret_cs.cruiseState.enabled or ret_cs.gasPressed:
       self.timer_resume = 50
-    elif v_ego_kph <= 0.1 and  modelxDistance > 30:
+    elif v_ego_kph <= 0.1 and  self.modelxDistance > 30:
       if self.timer_resume <= 0:
         events.add( EventName.chimeAtResume )
     else:
