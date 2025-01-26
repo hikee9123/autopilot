c76d84b46b1548ffddab7836b245d840acc47fe7
bhc-suhwoo <bhc@suhwoo.com>
2025-01-26 13:27:12 +0900

    fix car

--- opendbc_repo/opendbc/car/hyundai/custom/interface.py
+++ opendbc_repo/opendbc/car/hyundai/custom/interface.py
@@ -1,13 +1,13 @@
 from cereal import car
 from openpilot.common.params import Params
-from openpilot.selfdrive.car import get_safety_config
+from opendbc.car import get_safety_config
 
 
 
 
 
 def get_params( ret, candidate ):
-  params = Params()  
+  params = Params()
   disengage_on_accelerator = params.get_bool("DisengageOnAccelerator")
 
   if not disengage_on_accelerator:
