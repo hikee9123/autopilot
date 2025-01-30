33855ed3b9421f570444c8291363ee1d780654c7
bhc-suhwoo <bhc@suhwoo.com>
2025-01-30 16:40:01 +0900

    #curv speed control

--- opendbc_repo/opendbc/car/hyundai/custom/navicontrol.py
+++ opendbc_repo/opendbc/car/hyundai/custom/navicontrol.py
@@ -254,8 +254,9 @@ class NaviControl():
       self.speed_plan_kps = speeds[-1] * CV.MS_TO_KPH
 
       #curv speed control
-      spd_curv = np.interp( abs(self.modelyDistance), [10, 60], [ 0, 10 ] )
-      self.speed_plan_kps -= spd_curv
+      if (self.cruise_set_mode & 4) and  (self.speed_plan_kps > 80):
+        spd_curv = np.interp( abs(self.modelyDistance), [10, 60], [ 0, 10 ] )
+        self.speed_plan_kps -= spd_curv
 
 
     if self.sm.updated["uICustom"]:
