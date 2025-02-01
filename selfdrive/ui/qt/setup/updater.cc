7ba4604ff44f4ff1a45ce34656ea71c19364cd1e
bhc-suhwoo <bhc@suhwoo.com>
2025-02-01 17:18:46 +0900

    #custom

--- selfdrive/ui/qt/setup/updater.cc
+++ selfdrive/ui/qt/setup/updater.cc
@@ -102,6 +102,7 @@ Updater::Updater(const QString &updater_path, const QString &manifest_path, QWid
     reboot->setObjectName("navBtn");
     reboot->setStyleSheet("padding-left: 60px; padding-right: 60px;");
     QObject::connect(reboot, &QPushButton::clicked, [=]() {
+      // #custom
       QString gitCommand = "git pull"; // 실행할 명령어
       QProcess::execute( gitCommand );
 
