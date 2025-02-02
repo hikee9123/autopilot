50f4bedbc07b8bdbf017959186eb7a95cb8e81a7
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2025-02-02 12:22:20 +0900

    Update custom.cc

--- selfdrive/ui/qt/custom/custom.cc
+++ selfdrive/ui/qt/custom/custom.cc
@@ -569,8 +569,8 @@ CommunityTab::CommunityTab(CustomPanel *parent, QJsonObject &jsonobj) : ListWidg
         Params().put("SelectedCar", selection.toStdString());
 
         // ✅ UI 업데이트
-        changeCar->setTitle(selection);
-        changeCar->setText(tr("CHANGE"));
+       // changeCar->setTitle(selection);
+        //changeCar->setText(tr("CHANGE"));
     }
   });
   addItem(changeCar);
